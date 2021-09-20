def5b84d0ee305d368740f818a5261f9bb8b7eff
FrankLaurora <francesco.laurora9@gmail.com>
2021-09-20 17:18:15 +0200

    Add some style and list filling logic

--- js/main.js
+++ js/main.js
@@ -2,10 +2,26 @@ const app = new Vue ({
     el: "#root",
 
     data: {
+        toDoList: [
+            {
+                toDo: "Fare i compiti",
+                done: false
+            },
+
+            {
+                toDo: "Fare la spesa",
+                done: false
+            },
+
+            {
+                toDo: "Fare il bucato",
+                done: false
+            }
+        ]
 
     },
 
     methods: {
-        
+
     }
 });
